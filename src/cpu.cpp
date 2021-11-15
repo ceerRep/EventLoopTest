4ffcaaf3fa6e4a6c678425f438e8bc090df64e71
ceerrep <ceerrep@gmail.com>
2021-11-15 15:45:03 +0800

    numa

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -3,12 +3,17 @@
 #include "cpu.hh"
 
 #include <sched.h>
+#include <thread>
+
+static int cores = std::thread::hardware_concurrency();
+
+#define TRANSFORM(x) ((x) >= cores / 2 ? (x) * 2 : ((x) - cores / 2) * 2 + 1)
 
 int assignToThisCore(int core_id)
 {
     thread_local cpu_set_t mask;
     CPU_ZERO(&mask);
-    CPU_SET(core_id, &mask);
+    CPU_SET(TRANSFORM(core_id), &mask);
     return sched_setaffinity(0, sizeof(mask), &mask);
 }
 
@@ -19,6 +24,6 @@ int assignToCores(int begin, int end)
     CPU_ZERO(&mask);
 
     for (int core_id = begin; core_id < end; core_id++)
-        CPU_SET(core_id, &mask);
+        CPU_SET(TRANSFORM(core_id), &mask);
     return sched_setaffinity(0, sizeof(mask), &mask);
 }
